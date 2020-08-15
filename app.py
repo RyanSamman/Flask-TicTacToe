e434d14ababf2e3e92fdf432852c5be4f751b2c2
RyanSamman <RyanSamman@outlook.com>
2020-08-15 21:27:10 +0300

    Remove date

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ class GameData(db.Model):
 
 	# For Debugging:
 	def __repr__(self):
-		data = json.dumps({"id": self.id, "date": self.date, 'moves': self.moves, 'win': self.win, 'draw': self.draw})
+		data = json.dumps({"id": self.id, 'moves': self.moves, 'win': self.win, 'draw': self.draw})
 		return f"<GameData {data}>"
 
 
